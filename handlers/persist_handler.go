f428c58f704082239470396b891b79473a3a155f
Philippe Moneda <philippemoneda@gmail.com>
2018-06-29 11:56:35 -0300

    inicio maestro

---
+++ handlers/persist_handler.go
@@ -0,0 +1,10 @@
+package handlers
+
+import (
+	"github.com/PMoneda/carrot"
+)
+
+//PersistHandler handle message from persist events
+func PersistHandler(context *carrot.MessageContext) error {
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/ONSBR/Plataforma-Maestro/handlers"
+	"github.com/PMoneda/carrot"
+)
+
+const persistQueue string = "event.persist.queue"
+
+func main() {
+	done := make(chan bool)
+	config := carrot.ConnectionConfig{
+		Host:     "localhost",
+		Username: "guest",
+		Password: "guest",
+		VHost:    "plataforma_v1.0",
+	}
+	conn, _ := carrot.NewBrokerClient(&config)
+
+	builder := carrot.NewBuilder(conn)
+	builder.DeclareTopicExchange("reprocessing_stack")
+
+	subConn, _ := carrot.NewBrokerClient(&config)
+
+	subscriber := carrot.NewSubscriber(subConn)
+
+	subscriber.Subscribe(carrot.SubscribeWorker{
+		Queue:   persistQueue,
+		Scale:   15,
+		Handler: handlers.PersistHandler,
+		AutoAck: true,
+	})
+	<-done
+}
